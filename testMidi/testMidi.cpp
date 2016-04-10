b02124c3fd3be2546228b11a1188067e51a1e367
antoine <antoine@28eb8d37-f890-1e4f-99d2-a32480d331fc>
2016-04-10 21:17:19 +0000

    - Moved partial code to varlength.h for handling variable length fwrites - Created miditypes.h for handling uintxx_t base types - Created tests for varlength.h - Fixed issues with variable length variables.
    git-svn-id: http://localhost/svn/libMidi/trunk@10 28eb8d37-f890-1e4f-99d2-a32480d331fc

--- testMidi/testMidi.cpp
+++ testMidi/testMidi.cpp
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <tchar.h>
 #include "libMidi.h"
+#include "miditypes.h"
+#include "varlength.h"
 
 void testCDE()
 {
@@ -14,8 +16,44 @@ void testCDE()
   f.save("testCDE.output.mid");
 }
 
+void testVariableLength()
+{
+  //0
+  {
+    FILE * f = fopen("0.output.bin", "wb");
+    unsigned int value = 0;
+    fwriteVariableLength(value, f);
+    fclose(f);
+  }
+
+  //127
+  {
+    FILE * f = fopen("127.output.bin", "wb");
+    unsigned int value = 127; // 0x7F
+    fwriteVariableLength(value, f);
+    fclose(f);
+  }
+
+  //255
+  {
+    FILE * f = fopen("255.output.bin", "wb");
+    unsigned int value = 255; // 0xFF
+    fwriteVariableLength(value, f);
+    fclose(f);
+  }
+
+  //32768
+  {
+    FILE * f = fopen("32768.output.bin", "wb");
+    unsigned int value = 32768; // 0x8000
+    fwriteVariableLength(value, f);
+    fclose(f);
+  }
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+  testVariableLength();
   testCDE();
 
   return 0;
