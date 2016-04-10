c2eb8e71180ba04429128a747e13e7e6a42b6703
antoine <antoine@28eb8d37-f890-1e4f-99d2-a32480d331fc>
2016-04-10 17:34:38 +0000

    - Created first test testCDE()
    git-svn-id: http://localhost/svn/libMidi/trunk@5 28eb8d37-f890-1e4f-99d2-a32480d331fc

--- testMidi/testMidi.cpp
+++ testMidi/testMidi.cpp
@@ -5,13 +5,18 @@
 #include <tchar.h>
 #include "libMidi.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+void testCDE()
 {
   MidiFile f;
   f.addNote(262, 100); //C4
   f.addNote(294, 100); //D4
   f.addNote(330, 100); //E4
-  f.save("output.mid");
-  return 0;
+  f.save("cde.output.mid");
 }
 
+int _tmain(int argc, _TCHAR* argv[])
+{
+  testCDE();
+
+  return 0;
+}
