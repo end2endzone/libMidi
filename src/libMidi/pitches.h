5e05654c3147b23fd2abd21ab411c9747abd4af3
Antoine <end2endzone@gmail.com>
2018-11-23 16:34:21 -0500

    Removed LGPL header in source files.

--- src/libMidi/pitches.h
+++ src/libMidi/pitches.h
@@ -1,34 +1,3 @@
-//
-//  libMidi Library - v1.0 - 04/15/2016
-//  Copyright (C) 2016 Antoine Beauchamp
-//  The code & updates for the library can be found on http://end2endzone.com
-//
-// AUTHOR/LICENSE:
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 3.0 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License (LGPL-3.0) for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
-// DISCLAIMER:
-//  This software is furnished "as is", without technical support, and with no 
-//  warranty, express or implied, as to its usefulness for any purpose.
-//
-// PURPOSE:
-//  This libMidi library allows one to create basic midi file based on
-//  tone frequency & duration.
-//
-// HISTORY:
-// 04/15/2016 v1.0 - Initial release.
-//
 
 #ifndef MIDIPITCHES_H
 #define MIDIPITCHES_H
